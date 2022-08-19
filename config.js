2c3d228813b93bafcbcdf5924001c48c55638dde
ViniciusMoreno123 <vinisrocha8@gmail.com>
2022-08-19 15:23:54 -0400

    pro71

---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCRiW13uGl1auHmvnzdnKQfAfprVZXAq-k",
+  authDomain: "pro71-928b3.firebaseapp.com",
+  projectId: "pro71-928b3",
+  storageBucket: "pro71-928b3.appspot.com",
+  messagingSenderId: "251775107701",
+  appId: "1:251775107701:web:b8ff371b500de0569dae3c"
+  
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
